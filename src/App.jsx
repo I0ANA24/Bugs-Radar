5ea55fa885c046499f834192fba158bffb5085c5
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-09-19 22:22:37 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className="w-screen flex flex-col items-center">
+      <Header />
       
     </div>
   )
 }
 
 export default App
+
+// 1h35min
